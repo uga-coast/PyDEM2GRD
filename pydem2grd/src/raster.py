834b719376fa6581561ac320488c6a26c57e1789
Matt Bilskie <mbilskie@uga.edu>
2022-12-28 09:40:42 -0500

    Modifying gdal to import from osgeo.

--- pydem2grd/src/raster.py
+++ pydem2grd/src/raster.py
@@ -5,7 +5,6 @@
 # M O D U L E S                                   
 #----------------------------------------------------------
 import numpy as np
-#import gdal
 from osgeo import gdal
 import rasterio
 from gdalconst import GA_ReadOnly
